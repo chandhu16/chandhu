b9c6d228312141b97fbdb9bc3dfc03391b620a5f
chandhu16 <61219690+chandhu16@users.noreply.github.com>
2020-03-12 12:11:48 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float r,a,c;
+printf("enter the value ofr\n");
+scanf("%f",&r);
+a=3.14*r*r;
+c=2*3.14*r;
+printf("Area of a circle=%f\n circumference=%f\n",a,c);
+}
